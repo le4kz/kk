9e75b177468dac0cf3b7380e2a2467b82dbe2832
le4kz <anas.elmo2013@outlook.com>
2025-07-13 15:22:40 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+bot.run(TOKEN)
